11b41e1d1a651a1db6693500daf56864adf42667
unknown <ilyessmoulazem1@gmail.com>
2023-07-13 02:43:21 +0200

    core

--- 02-python-oop/w01-d02/core/BankAccount.py
+++ 02-python-oop/w01-d02/core/BankAccount.py
@@ -1,9 +1,9 @@
-class BankAccount:
+class Bankaccount:
     accounts=[]
     def __init__(self, int_rate, balance=0):
         self.int_rate=int_rate
         self.balance=balance
-        BankAccount.accounts.append(self)
+        Bankaccount.accounts.append(self)
     
     def deposit(self, amount):
         self.balance+=amount
@@ -31,9 +31,9 @@ class BankAccount:
             account.display_account_info()
 
 # Example usage:
-account1 = BankAccount(0.01, 100)
-account1.deposit(50).deposit(100).deposit(60).withdraw(500).yield_interest().display_account_info()
-account2 = BankAccount(0.03, 50)
-account2.deposit(70).deposit(300).withdraw(50).withdraw(70).withdraw(20).withdraw(250).yield_interest().display_account_info()
-account3=BankAccount(0.03,300)
-BankAccount.print_all_accounts()
+account1 = Bankaccount(0.01, 100) 
+account1.deposit(50).deposit(100).deposit(60).withdraw(500).yield_interest().display_account_info() 
+account2 = Bankaccount(0.03, 50) 
+account2.deposit(70).deposit(300).withdraw(50).withdraw(70).withdraw(20).withdraw(250).yield_interest().display_account_info() 
+account3=Bankaccount(0.03,300) 
+Bankaccount.print_all_accounts()
